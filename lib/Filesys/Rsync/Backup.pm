d841092444fe1a8c2063f86a6662122ccdaefe20
Nikolay Martynov <mar.kolya@gmail.com>
2014-05-30 23:31:59 -0400

    Initial commit

---
+++ lib/Filesys/Rsync/Backup.pm
@@ -0,0 +1,19 @@
+package Filesys::Rsync::Backup;
+
+=head1 NAME
+
+Filesys::Rsync::Backup - A simple script to backup filesystems with rsync
+
+=head1 VERSION
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+TODO
+
+=cut
+
+1;
